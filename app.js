e176db1affda4cbd3016582f6b62be838deeebc3
Sean Brooks <seanbrooks121@gmail.com>
2019-04-20 13:41:21 +0100

    Added user model

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser')
 
 const errorController = require('./controllers/error')
 const mongoConnect = require('./util/database').mongoConnect
+const User = require('./models/user')
 
 const app = express()
 
@@ -19,13 +20,13 @@ const shopRoutes = require('./routes/shop')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-// app.use((req, res, next) => {
-//     User.findByPk(1)
-//         .then(user => {
-//             req.user = user
-//             next()
-//         }).catch(err => console.log(err))
-// })
+app.use((req, res, next) => {
+    User.findById('5cbafc1fb12c4a4b577f4e30')
+        .then(user => {
+            req.user = user
+            next()
+        }).catch(err => console.log(err))
+})
 
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
